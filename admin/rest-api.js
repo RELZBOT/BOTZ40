dbecef82ed801ea2268e6f61742ff69477f7528b
Naylachan <82255308+naylachan@users.noreply.github.com>
2022-10-08 06:25:45 +0800

    Add files via upload

---
+++ admin/rest-api.js
@@ -0,0 +1,10 @@
+/* Saya berterimakasih Kepada Penyedia Api */
+const api = require("caliph-api"); // https://www.npmjs.com/package/caliph-api
+const xa = require('xfarr-api')
+
+// REST-API
+// (1) pecundang.herokuapp.com => Benny
+
+module.exports = { api, xa }
+
+// [❗] JANGAN UBAH APA APA DISINI!
